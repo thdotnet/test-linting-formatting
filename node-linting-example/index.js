8f7d34402313f96f1dbc1b98803060e28c9b327f
Thiago Custodio <thxplode@gmail.com>
2024-07-16 14:24:38 -0500

    Update index.js

--- node-linting-example/index.js
+++ node-linting-example/index.js
@@ -1,9 +1,6 @@
-function testFunction() {
-  let a = 1
-  let b = 2
-  console.log(a + b)
-  console.log('result', a + b)
-}
+function testFunction(){
+let a=1;
+let b=2;console.log(a+b);console.log("result",a+b)}
 testFunction()
 
 //function testFunction(){
